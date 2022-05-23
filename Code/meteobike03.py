c851a875e6c7da868daa1277e51535bd21ada774
Dominik Strebel <strebdom@ethz.ch>
2022-05-23 13:59:40 +0200

    threading

--- Code/meteobike03.py
+++ Code/meteobike03.py
@@ -248,7 +248,8 @@ value_vappress = Label(master, text=" Vap. Pressure ",
                        font=('Helvetica', font_size))
 value_vappress.grid(row=10, column=1, sticky=W, columnspan=2)
 # initialize value_counter
-start_counting(value_counter)
+th = threading.Thread(target=start_counting, args=[value_counter])
+#start_counting(value_counter)
 # define buttons
 b1 = Button(master, text='Record', width=7,
             state=DISABLED, command=record_data)
